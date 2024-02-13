94063541b0e46bb1d1be72c626f707d5d0a0af4e
User#None <77580844+issamansur@users.noreply.github.com>
2024-02-13 15:20:15 +0200

    Changed logic of `Pawn` and `King`

--- ChessLibrary/ChessMaster/Figures/King.cs
+++ ChessLibrary/ChessMaster/Figures/King.cs
@@ -16,6 +16,7 @@ public class King: Figure
         // Can move if:
         // 1. AbsDiffX and AbsDiffY are both <= 1
         // 2. Castling
-        return move is { AbsDiffX: <= 1, AbsDiffY: <= 1 } || board.CanCastleTo(move);
+        return move is { AbsDiffX: <= 1, AbsDiffY: <= 1 } || 
+               move is { AbsDiffX: 2, DiffY: 0 } && board.CanCastleTo(move);
     }
 }
--- ChessLibrary/ChessMaster/Figures/Pawn.cs
+++ ChessLibrary/ChessMaster/Figures/Pawn.cs
@@ -10,15 +10,83 @@ public class Pawn: Figure
     public Pawn(Color color) : base(color)
     {
     }
+
+    public bool CanSimpleMove(Board board, Move move)
+    {
+        // Check on Direction
+        if (move is not ({ DiffX: 0 }
+            and ({ Figure.Color: Color.White, DiffY: 1 }
+            or { Figure.Color: Color.Black, DiffY: -1 }
+            )))
+            return false;
+
+        // Check on Capturing
+        if (!CanCapturing(move))
+        {
+            return false;
+        }
+
+        // if field on board is empty - true, else - false
+        return
+            board[move.To] is null;
+    }
+
+    public bool CanDoubleMove(Board board, Move move)
+    {
+        // if starting/end place correct - false
+        if (move is not ({ DiffX: 0 } 
+            and ({ Figure.Color: Color.White, From.Y: 1, To.Y: 3 } 
+            or { Figure.Color: Color.Black, From.Y: 6, To.Y: 4 }
+            )))
+           return false;
+
+        // if fields on board is empty - true, else - false
+        return
+            board[move.From + move.Direction] is null &&
+            board[move.To] is null;
+    }
+
+    public bool CanEat(Board board, Move move)
+    {
+        if (move is { AbsDiffX: 1, AbsDiffY: 1})
+
+        // Check on Capturing
+        if (!CanCapturing(move))
+        {
+            return false;
+        }
+
+        return
+            board[move.To] is not null;
+    }
+
+    public bool CanCapturing(Move move)
+    {
+        // if not last line
+        if (move.To is not { Y: 0 or 7 })
+        {
+            return move.CapturedFigure is null;
+        }
+
+        // if last line
+        if (move is { CapturedFigure: null or Pawn or King } ||
+            move.CapturedFigure.Color != move.Figure.Color)
+        {
+            return false;
+        }
+        return true;
+    }
     
     public override bool CanMove(Board board, Move move)
     {
         // Can move if:
         // 1. AbsDiffX is 0 and AbsDiffY is 1 (if on empty field)
+        // 1. AND IF IT IS LAST LINE - CHECK IF CAPTURING CORRECT
         // 2. AbsDiffX is 0 and AbsDiffY is 2 (if on starting position and the field in front is empty)
         // 3. AbsDiffX is 1 and AbsDiffY is 1 (if on non-empty field)
-        return move is { AbsDiffX: 0, AbsDiffY: 1 } && board[move.To] == null ||
-               move is { AbsDiffX: 0, AbsDiffY: 2, From.Y: 1 or 6 } && board[move.From + move.Direction] == null && board[move.To] == null ||
-               move is { AbsDiffX: 1, AbsDiffY: 1 } && board[move.To] != null;
+        return 
+            CanSimpleMove(board, move) || 
+            CanDoubleMove(board, move) ||
+            CanEat(board, move);
     }
 }
