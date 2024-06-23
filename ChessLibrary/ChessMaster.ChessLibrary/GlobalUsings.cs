c88741328cf11a8b9b568976fb98c05275781bc0
Issa <issa_mansur@mail.ru>
2024-06-24 02:43:04 +0300

    Fixed typo with definition of `ChessExt`

--- ChessLibrary/ChessMaster.ChessLibrary/GlobalUsings.cs
+++ ChessLibrary/ChessMaster.ChessLibrary/GlobalUsings.cs
@@ -1,12 +1,5 @@
 global using ChessMaster.ChessLibrary.Boards;
-global using ChessMaster.ChessLibrary.Exceptions;
 global using ChessMaster.ChessLibrary.Figures;
 global using ChessMaster.ChessLibrary.States;
 global using ChessMaster.ChessLibrary.Utils;
 
-namespace ChessMaster.ChessLibrary;
-
-public static class ChessExt
-{
-    public const string DefaultFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-}
---
+++ ChessLibrary/ChessMaster.ChessLibrary/Utils/ChessExt.cs
@@ -0,0 +1,6 @@
+namespace ChessMaster.ChessLibrary.Utils;
+
+public static class ChessExt
+{
+    public const string DefaultFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+}
